d9e13827f99515fc7924bde166b3fc1514241cd9
neelakandan <neelakandan2001kpm@gmail.com>
2024-03-07 08:23:22 +0530

    dT

--- Django/Model_demo/dbapp/models.py
+++ Django/Model_demo/dbapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    empno=models.CharField()
+    empno=models.IntegerField()
     empname=models.CharField(max_length=20)
     empsalary=models.IntegerField()
     address=models.CharField(max_length=50)
